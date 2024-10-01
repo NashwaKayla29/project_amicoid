2beef91d597d2d8a8170ae4be55f27d8be5dbe01
alvin <alvinpurwo03@gmail.com>
2024-10-01 11:24:52 +0700

    add seeder

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use App\Http\Controllers\BahanController;
 use App\Http\Controllers\BarangController;
-use App\Http\Controllers\PotongController;
 use App\Http\Controllers\HasilPotongController;
+use App\Http\Controllers\PotongController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,21 +16,25 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(
+    [
+        'register' => false,
+    ]
+);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Route::group(['prefix' => 'admin', 'middleware' => ['auth', IsAdmin::class]], function () {
-    // Route::resource('user', UsersController::class);
-    Route::resource('bahan', BahanController::class);
-    Route::resource('barang', BarangController::class);
-    Route::resource('potong', PotongController::class);
-    Route::resource('HasilPotong', HasilPotongController::class); 
+// Route::resource('user', UsersController::class);
+Route::resource('bahan', BahanController::class);
+Route::resource('barang', BarangController::class);
+Route::resource('potong', PotongController::class);
+Route::resource('HasilPotong', HasilPotongController::class);
 
 // });
